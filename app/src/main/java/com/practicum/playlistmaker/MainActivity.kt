08191113fc2c9b4d8947b08c2506d8cf2f5e3eda
Aleksey Putilov <putilov0407@gmail.com>
2022-12-24 20:42:02 +0500

    add Toast for button: Search, Media, Settings. fix dimens.xml for button_radius

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -2,10 +2,37 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Ссылки на кнопки на главном экране
+        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
+        val buttonMedia = findViewById<Button>(R.id.buttonMedia)
+        val buttonSettings = findViewById<Button>(R.id.buttonSettings)
+
+        //Заглушка на нажатие кнопки "Поиск" методом анонимного класса
+        val buttonSearchClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "Бегу искать музыку", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        //Заглушка на нажатие кнопки "Медиатека" методом лямбда-выражение
+        buttonMedia.setOnClickListener() {
+            Toast.makeText(this@MainActivity, "Открываю вашу любимую музыку", Toast.LENGTH_SHORT).show()
+        }
+
+        //Заглушка на нажатие кнопки "Настройки" методом лямбда-выражение
+        buttonSettings.setOnClickListener() {
+            Toast.makeText(this@MainActivity, "Сейчас все настроим", Toast.LENGTH_SHORT).show()
+        }
     }
 }
