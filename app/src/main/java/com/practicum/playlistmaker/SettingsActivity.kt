9a38c32dec622ab895ff54fc2e923aac4bb2a385
Aleksey Putilov <putilov0407@gmail.com>
2023-01-17 19:24:13 +0500

    Homework 9.2 implement the button share, support, user_agreement

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -16,6 +18,39 @@ class SettingsActivity : AppCompatActivity() {
         // на главный экран через закрытие экрана "Настройки"
         buttonArrowBackSettings.setOnClickListener(){
             finish()
+        }
+
+        //Ссылки на кнопки
+        val share = findViewById<ImageView>(R.id.share)
+        val support = findViewById<ImageView>(R.id.support)
+        val userAgreement = findViewById<ImageView>(R.id.user_agreement)
+
+        //Реализация кнопки "Поделиться приложением"
+        share.setOnClickListener() {
+            val linkToTheCourse = "https://practicum.yandex.ru/android-developer/"
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain")
+            shareIntent.putExtra(Intent.EXTRA_TEXT, linkToTheCourse)
+            startActivity(shareIntent)
+        }
+
+        //Реализация кнопки "Поддержка"
+        support.setOnClickListener(){
+            val message = getString(R.string.message)
+            val subject = getString(R.string.subject)
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("putilov0407@gmail.com"))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+
+        //Реализация кнопки пользовательское соглашение
+        userAgreement.setOnClickListener(){
+            val uriUserAgreement = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, uriUserAgreement)
+            startActivity(intent)
         }
     }
 }
