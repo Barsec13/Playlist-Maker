5df6d9676340f48a4d11d00f5c1e821c0e053153
Aleksey Putilov <putilov0407@gmail.com>
2023-01-18 20:56:49 +0500

    Homework 9.3 add layout for SearchActivity, logic for searchClearIcon, TextWatcher

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,69 @@
 package com.practicum.playlistmaker
-
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        //Кнопка "<-" из окна "Настройки"
+        val buttonArrowBackSettings = findViewById<Button>(R.id.buttonArrowBackSettings)
+        //Обработка нажатия на кнопку "<-" и переход
+        // на главный экран через закрытие экрана "Настройки"
+        buttonArrowBackSettings.setOnClickListener(){
+            finish()
+        }
+
+        //Ссылки на элементы
+        val searchClearIcon = findViewById<ImageView>(R.id.searchClearIcon)
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+
+        //Объект для работы с клавиатурой
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+
+        //Очистить поле для ввода и скрыть клавиатуру
+        searchClearIcon.setOnClickListener {
+            searchEditText.setText("")
+            inputMethodManager?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        }
+
+        //Работа с вводимым текстом
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            //Действие при вводе текста
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                searchClearIcon.visibility = searchClearIconVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+
+        //Связть поля для ввода и TextWatcher
+        searchEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    //Скрыть или показать кнопку для сброса ввода
+    private fun searchClearIconVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
+
