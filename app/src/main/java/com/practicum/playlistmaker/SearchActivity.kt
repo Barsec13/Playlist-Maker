6b7b4b7df369e6cda2ca75c250e80ef5d8cc84ce
Aleksey Putilov <putilov0407@gmail.com>
2023-01-20 20:03:38 +0500

    Homework 9.4 add save a search query SearchActivity

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -12,10 +12,10 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 
 class SearchActivity : AppCompatActivity() {
+    var textSearch = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-
         //Кнопка "<-" из окна "Настройки"
         val buttonArrowBackSettings = findViewById<Button>(R.id.buttonArrowBackSettings)
         //Обработка нажатия на кнопку "<-" и переход
@@ -27,6 +27,7 @@ class SearchActivity : AppCompatActivity() {
         //Ссылки на элементы
         val searchClearIcon = findViewById<ImageView>(R.id.searchClearIcon)
         val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        searchEditText.setText(textSearch)
 
         //Объект для работы с клавиатурой
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
@@ -46,6 +47,7 @@ class SearchActivity : AppCompatActivity() {
             //Действие при вводе текста
             override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 searchClearIcon.visibility = searchClearIconVisibility(s)
+                textSearch = "searchEditText.getText().toString()"
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -64,6 +66,21 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    companion object {
+        const val TEXT_SEARCH = "TEXT_SEARCH"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT_SEARCH,textSearch)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textSearch = savedInstanceState.getString(TEXT_SEARCH).toString()
+        findViewById<EditText>(R.id.searchEditText).setText(textSearch)
     }
 }
 
